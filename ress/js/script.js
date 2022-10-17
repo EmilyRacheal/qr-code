d80ca01c5ba309f28237a2d327cd72ae5e3824ad
emily <emilychealy4@gmail.com>
2022-10-17 15:08:08 +0100

    validating the form input

---
+++ ress/js/script.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+
+
+// function and event listner for geting the value of the input
+const onGenerateSubmit = (e) =>{
+    e.preventDefault();
+
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    if (url === '') {
+        alert('enter a url')
+    }
+    else{
+        showSpinner()
+    }
+
+    console.log(url, size)
+}
+
+
+// function for hide and show spinner
+const showSpinner = () =>{
+    document.getElementById("spinner").style.display="block"
+}
+
+const hideSpinner = () =>{
+    document.getElementById("spinner").style.display="none"
+}
+
+hideSpinner();
+
+
+form.addEventListener('submit', onGenerateSubmit)
